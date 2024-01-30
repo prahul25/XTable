f80edf86cc1a94f0b355740b07b3508de1f45e2a
prahul25 <palrahulkumar2002@gmail.com>
2024-01-30 19:46:59 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 
-
+const staticData = [
+  { date: "2022-09-01", views: 100, article: "Article 1" },
+  { date: "2023-09-01", views: 100, article: "Article 1" },
+  { date: "2023-09-02", views: 150, article: "Article 2" },
+  { date: "2023-09-02", views: 120, article: "Article 3" },
+  { date: "2020-09-03", views: 200, article: "Article 4" }
+  ]
 function App() {
   const [data, setData] = useState([
     { date: "2022-09-01", views: 100, article: "Article 1" },
@@ -8,11 +14,11 @@ function App() {
     { date: "2023-09-02", views: 150, article: "Article 2" },
     { date: "2023-09-02", views: 120, article: "Article 3" },
     { date: "2020-09-03", views: 200, article: "Article 4" }
-    ]);
+    ])
   console.log(data)
 
   const sortByDate = () => {
-    const sortedByDate = data.sort((a, b) => {
+    const sortedByDate = staticData.sort((a, b) => {
       return new Date(b.date) - new Date(a.date);
     });
     setData(sortedByDate)
@@ -20,7 +26,7 @@ function App() {
   }
   
   const sortByViews = () => {
-    const sortedByView = data.sort((a, b) => b.views - a.views);
+    const sortedByView = staticData.sort((a, b) => b.views - a.views);
     setData(sortedByView)
     console.log(data)
   }
