7d1c21ac09f74196d5442a5b46be2ec62332613f
prahul25 <palrahulkumar2002@gmail.com>
2024-01-30 19:32:23 +0530

    Project added

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+
+function App() {
+  const [data, setData] = useState([
+    { date: "2022-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-02", views: 150, article: "Article 2" },
+    { date: "2023-09-02", views: 120, article: "Article 3" },
+    { date: "2020-09-03", views: 200, article: "Article 4" }
+    ]);
+  console.log(data)
+
+  const sortByDate = () => {
+    const sortedByDate = data.sort((a, b) => {
+      return new Date(b.date) - new Date(a.date);
+    });
+    setData(sortedByDate)
+    console.log(data)
+  }
+  
+  const sortByViews = () => {
+    const sortedByView = data.sort((a, b) => b.views - a.views);
+    setData(sortedByView)
+    console.log(data)
+  }
+
+  return (
+    <div>
+      <h1>Date and Views Table</h1>
+      <button onClick={sortByDate}>Sort by Date</button>
+      <button onClick={sortByViews}>Sort by Views</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Views</th>
+            <th>Article</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((entry, idx) => (
+            <tr key={idx}>
+              <td>{entry.date}</td>
+              <td>{entry.views}</td>
+              <td>{entry.article}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
